8bf7569f036de5745f884171a03720622eda45a4
kwegorek <ka.wegorek@gmail.com>
2019-09-25 16:36:33 -0400

    commiting query

--- Routes/posts.js
+++ Routes/posts.js
@@ -7,15 +7,19 @@ const postDetails = require("../views/postDetails");
 
 router.get("/", async (req, res, next) => {
   try {
-    const data = await client.query(/*Omitted for brevity*/);
+    const data = await client.query(`SELECT posts.*, counting.upvotes 
+    FROM posts 
+    INNER JOIN (SELECT postId, COUNT(*) as upvotes FROM upvotes GROUP BY postId) AS counting
+    ON posts.id = counting.postId`);
     res.send(postList(data.rows));
   } catch (error) { next(error) }
 });
 
 router.get("/:id", async (req, res) => {
   try {
-    const data = await client.query(/*Omitted for brevity*/);
-    const post = data.rows[0];
+    const postId = req.params.id;
+    const data = await client.query(`SELECT title, name, content FROM posts JOIN users ON users.id = userid  WHERE posts.id = ${postId}`);
+    const [post] = data.rows;
     res.send(postDetails(post));
   } catch (error) { next(error) }
 });
